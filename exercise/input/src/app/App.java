489a49012442c3a1febccb0d8c3b213bfc62e335
Myles Knowlin <117530083+Makinist08@users.noreply.github.com>
2026-05-03 15:11:01 -0400

    Ordinal

--- exercise/input/src/app/App.java
+++ exercise/input/src/app/App.java
@@ -6,11 +6,28 @@ public class App {
     public static void main(String[] args) throws Exception {
         InputHandler inputHandler = InputHandler.getInstance();
 
-        while(true){
-            System.out.println( "Let me add 2 to that " + (inputHandler.requestBoundedInt(0, 100) + 2));
+        System.out.println( "Let me add 2 to that " + (inputHandler.requestBoundedInt(0, 100) + 2));
 
-            char c = inputHandler.requestBoundedChar('a', 'z');
-            System.out.println(c + " is the " + (Character.getNumericValue(c) - 9) + " letter in the alphabet");
+        char c = inputHandler.requestBoundedChar('a', 'z');
+        System.out.println(c + " is the " + ordinal(Character.getNumericValue(c) - 9) + " letter in the alphabet");
+
+    }
+
+    // Source - https://stackoverflow.com/a/6810409
+    // Posted by Bohemian, modified by community. See post 'Timeline' for change history
+    // Retrieved 2026-05-03, License - CC BY-SA 4.0
+
+    public static String ordinal(int i) {
+        String[] suffixes = new String[] { "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th" };
+        switch (i % 100) {
+        case 11:
+        case 12:
+        case 13:
+            return i + "th";
+        default:
+            return i + suffixes[i % 10];
+
         }
     }
+
 }
